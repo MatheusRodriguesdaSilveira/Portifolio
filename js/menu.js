e79c39aa2f93af7371c123696fb9d8a0bccfb9d1
MatheusRodriguesdaSilveira <matheusrsplay1996@gmail.com>
2024-08-01 14:44:06 -0300

    Text Animation

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,27 @@
+var titleText = document.getElementById('dynamic_text');
+var text = 'SEARCH FOR SUCCESS'; // Sem o ponto final
+var point = '.'; // Ponto final estilizado
+var index = 0;
 
-document.getElementById('menu-icon').addEventListener('click', function () {
-    const nav = document.querySelector('nav');
-    nav.classList.toggle('hidden');
-});
+const write = () => {
+    titleText.innerHTML = titleText.innerHTML.replace('|', '')
 
+    if (text.length > index) {
+        titleText.innerHTML += text.charAt(index) + '|';
+        index++;
+        setTimeout(write, 100);
+    } else if (index === text.length) {
+        titleText.innerHTML += '<span class="text-ocean text-5xl md:text-7xl">' + point + '</span>' + '|';
+        index++;
+        setTimeout(write, 100);
+    } else {
+        titleText.innerHTML = text + '<span class="text-ocean text-5xl md:text-7xl">' + point + '</span>';
+        setTimeout(() => {
+            index = 0;
+            titleText.innerHTML = ''; 
+            write();
+        }, 1000);
+    }
+}
+
+write();
