1bd9b9980e5e348cfe6146a547d5f3e1ab7b569f
MatheusRodriguesdaSilveira <matheusrsplay1996@gmail.com>
2024-08-06 16:28:27 -0300

    Atualizações

--- js/menu.js
+++ js/menu.js
@@ -4,23 +4,20 @@ var point = '.'; // Ponto final estilizado
 var index = 0;
 
 const write = () => {
-    titleText.innerHTML = titleText.innerHTML.replace('|', '')
+    titleText.innerHTML = titleText.innerHTML.replace('|', '');
 
     if (text.length > index) {
         titleText.innerHTML += text.charAt(index) + '|';
         index++;
-        setTimeout(write, 100);
+        setTimeout(write, 150);
     } else if (index === text.length) {
-        titleText.innerHTML += '<span class="text-ocean text-5xl md:text-7xl">' + point + '</span>' + '|';
+        titleText.innerHTML += '<span class="text-ocean">' + point + '</span>';
         index++;
-        setTimeout(write, 100);
-    } else {
-        titleText.innerHTML = text + '<span class="text-ocean text-5xl md:text-7xl">' + point + '</span>';
         setTimeout(() => {
             index = 0;
             titleText.innerHTML = ''; 
             write();
-        }, 1000);
+        }, 800);
     }
 }
 
