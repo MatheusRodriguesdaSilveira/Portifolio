a8950ae37a3f767e1668623b6942858fb3ffe419
MatheusRodriguesdaSilveira <matheusrsplay1996@gmail.com>
2024-08-30 15:00:16 -0300

    #editing

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,23 @@
-var titleText = document.getElementById('dynamic_text');
-var text = 'SEARCH FOR SUCCESS'; // Sem o ponto final
-var point = '.'; // Ponto final estilizado
-var index = 0;
+// var titleText = document.getElementById('dynamic_text');
+// var text = 'FRONTEND DEVELOPER'; // Sem o ponto final
+// var point = '.'; // Ponto final estilizado
+// var index = 0;
 
-const write = () => {
-    titleText.innerHTML = titleText.innerHTML.replace('|', '');
+// const write = () => {
+//     titleText.innerHTML = titleText.innerHTML.replace('|', '');
 
-    if (text.length > index) {
-        titleText.innerHTML += text.charAt(index) + '|';
-        index++;
-        setTimeout(write, 180);
-    } else if (index === text.length) {
-        titleText.innerHTML += '<span class="bg-gradient-to-r text-white bg-clip-text text-transparent">' + point + '</span>';
-        index++;
-        setTimeout(() => {
-            index = 0;
-            titleText.innerHTML = ''; 
-            write();
-        }, 700);
-    }
-}
+//     if (text.length > index) {
+//         titleText.innerHTML += text.charAt(index) + '|';
+//         index++;
+//         setTimeout(write, 180);
+//     } else if (index === text.length) {
+//         index++;
+//         setTimeout(() => {
+//             index = 0;
+//             titleText.innerHTML = ''; 
+//             write();
+//         }, 700);
+//     }
+// }
 
-write();
+// write();
